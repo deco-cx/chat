2581ea2d6ed5f8c1ebe5fdb5ec499bca34775275
gimenes <tlgimenes@gmail.com>
2025-10-17 15:28:29 -0300

    gs

--- apps/web/src/components/layout/decopilot-layout.tsx
+++ apps/web/src/components/layout/decopilot-layout.tsx
@@ -1,16 +1,9 @@
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@deco/ui/components/resizable.tsx";
 import { PropsWithChildren } from "react";
-import { useLocation } from "react-router";
 import { useLocalStorage } from "../../hooks/use-local-storage";
 import {
   DecopilotContextValue,
   DecopilotProvider,
 } from "../decopilot/context.tsx";
-import { DecopilotChat } from "../decopilot/index.tsx";
 
 export function useDecopilotOpen() {
   const { value: open, update: setOpen } = useLocalStorage({
@@ -29,28 +22,20 @@ export function useDecopilotOpen() {
   };
 }
 
+/**
+ * DecopilotLayout is now a simple context provider for pages that need to
+ * provide additional context to the Decopilot chat.
+ * 
+ * The actual chat rendering is handled by ProjectLayout to prevent re-renders
+ * during navigation.
+ */
 export function DecopilotLayout({
   children,
   value,
 }: PropsWithChildren<{ value: DecopilotContextValue }>) {
-  const { open: decopilotOpen } = useDecopilotOpen();
-  const location = useLocation();
-  const isAgentDetailPage = location.pathname.match(/\/agent\/[^\/]+\/[^\/]+$/);
-
   return (
     <DecopilotProvider value={value}>
-      <ResizablePanelGroup direction="horizontal">
-        <ResizablePanel>{children}</ResizablePanel>
-        {/* Don't show DecopilotChat panel on agent detail pages (handled internally) */}
-        {decopilotOpen && !isAgentDetailPage && (
-          <>
-            <ResizableHandle withHandle />
-            <ResizablePanel defaultSize={30}>
-              <DecopilotChat />
-            </ResizablePanel>
-          </>
-        )}
-      </ResizablePanelGroup>
+      {children}
     </DecopilotProvider>
   );
 }
